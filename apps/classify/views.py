f8ce114bebde94cfd883fd630a111b0ff81af29c
h-hosoda-ml <h.hosoda.ml@gmail.com>
2023-09-09 18:20:56 +0900

    add: キャンバスと描画処理関数の作成

--- apps/classify/views.py
+++ apps/classify/views.py
@@ -1,5 +1,5 @@
-from flask import Blueprint, render_template
-
+from flask import Blueprint, render_template, request, jsonify
+from flask_login import login_required
 
 # classifyアプリ
 classify = Blueprint(
@@ -11,3 +11,13 @@ classify = Blueprint(
 @classify.route("/")
 def index():
     return render_template("classify/index.html")
+
+
+# digitsdrawエンドポイントの作成
+@classify.route("/digitsdraw", methods=["GET", "POST"])
+@login_required
+def digitsdraw():
+    if request.method == "POST":
+        # TODO: cnnモデルによる予測を行い結果を返す処理の作成
+        pass
+    return render_template("classify/digitsdraw.html")
